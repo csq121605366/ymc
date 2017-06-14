3d0f8f4ca64a91a2ffc1a158893d5bd24db5da06
caishangqing <121605366@qq.com>
2017-06-14 21:46:41 +0800

    家里的

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import { Button, Select } from 'element-ui'
+import '../theme/index.css'
+
 import Vue from 'vue'
-import App from './App'       // 路由挂载
+import App from './App' // 路由挂载
 import router from './router/route' // 路由列表
 import store from './store/store'
 
-import './assets/less/index.less'
-
+Vue.use(Button)
 Vue.config.productionTip = false
-
-
 
 // eslint单行验证
 /* eslint-disable no-new */
 
 new Vue({
-  // el: '#app',
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
-
-
+})
